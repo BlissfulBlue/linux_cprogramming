4bf90695d70153b516070e55fc1ff815bad00b32
BlissfulBlue <62179850+BlissfulBlue@users.noreply.github.com>
2022-08-02 16:44:03 -0400

    Update awsinstall.sh
    The old script for installing aws cli and launching an instance

--- awsinstall.sh
+++ awsinstall.sh
@@ -20,3 +20,28 @@ unzip awscliv2.zip
   /usr/local/bin/aws --version
   #set variable for path
   export PATH=$PATH:/usr/local/bin/
+
+# aws credentials
+aws configure
+
+#aws ec2 describe-instances --region us-east-1 | grep "PrivateIpAddress"
+#aws ec2 describe-instances --region us-west-1 | grep "PrivateIpAddress"
+#aws ec2 describe-instances --region us-south-1 | grep "PrivateIpAddress"
+#aws ec2 describe-instances --region us-north-1 | grep "PrivateIpAddress"
+
+echo   export PATH=$PATH:/usr/local/bin/ >>~/.bashrc
+
+echo   export PATH=$PATH:/usr/local/bin/ >>/home/admin/.bashrc
+
+#filter out to ip addresses
+git config --global credential.helper store
+
+# erase ipad.json
+rm ipad.json
+
+#git clone https://gitlab.com/discord-fiends/roy-rogers.git
+aws ec2 run-instances --image-id ami-04a32162efe87cb4c --count 1 --instance-type t2.micro --key-name JESSEANDREWKEY --security-group-ids >>ipad.json
+cat ipad.json | grep "PrivateIpAddress"
+
+# check status
+aws ec2 describe-instances --region us-west-2 | grep "PrivateIpAddress"
