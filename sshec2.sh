d4a1b9dcf5d83801853a9c49c8f075b569d54383
BlissfulBlue <62179850+BlissfulBlue@users.noreply.github.com>
2022-08-10 17:59:13 -0400

    Update sshec2.sh
    Clean up code, gives instance extra startup time.

--- sshec2.sh
+++ sshec2.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
-# this is redhat ami
+# this is centos 7 ami
 AMI=ami-0686851c4e7b1a8e1
 USER=centos
+
 # this is ubuntu
 # AMI=ami-04a32162efe87cb4c
 # USER=ubuntu
+
+# instance type
+# typeinstance=t2.micro
+
+typeinstance=t2.2xlarge
+
+# typeinstance=t3.medium
+
+# personal key
+# privkey=ANDREWJESSEKEY.pem
+
 # remove ipad file
 rm ipad.json
 
 # create new instance
-aws ec2 run-instances --image-id $AMI --count 1 --instance-type t2.micro --key-name JESSEANDREWKEY --security-group-ids >>ipad.json
+aws ec2 run-instances --image-id $AMI --count 1 --instance-type $typeinstance --key-name JESSEANDREWKEY --security-group-ids >>ipad.json
 
 # find instance ID
 echo "hi were getting your new instance id"
@@ -27,11 +39,10 @@ do
 	source getpublicipaws.sh $INSTANCEID
 echo ip: $PUBLICIP
 done
-echo "Please wait for the vm to start before it can connect this will take 60 seconds"
-sleep 50
+echo "Please wait for the vm to start before it can connect this will take 80 seconds"
+sleep 70
 echo ssh -i $1 $USER@$PUBLICIP
 scp -o StrictHostKeyChecking=no -i $1 $2 $USER@$PUBLICIP:~/Provision.sh
 ssh -o StrictHostKeyChecking=no -i $1 $USER@$PUBLICIP "sudo bash Provision.sh"
-echo "hey were trying to connect but its going to take a little more time then expected"
-sleep 20
-ssh -i $1  $USER@$PUBLICIP
+ssh -o StrictHostKeyChecking=no -i $1  $USER@$PUBLICIP
+
